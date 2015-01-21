1db508916acbeb98e271782a8d72faef4cd488a8
fxl0206 <fxl0206@gmail.com>
2015-01-21 11:18:03 +0800

    add html

--- src/build/build.sh
+++ src/build/build.sh
@@ -4,7 +4,7 @@ GOPATH=`pwd`
 export GOPATH
 cd $GOPATH
 git pull
-cp -R $GOPATH/src/views $IOUT
+cp -R $GOPATH/src/html $IOUT
 cp -R $GOPATH/src/conf $IOUT
 go build -o $IOUT/yxfs $GOPATH/src/main.go
 chmod +x $GOPATH/src/build/*.sh
